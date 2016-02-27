7fca1247a11e093714a0415c1482c431bdcd4c09
James McDonnell <topgamer7@gmail.com>
2016-02-27 09:22:56 -0800

    Added EntityFramework logic.

---
+++ AdminBackend/MBBackEnd/MBBackEnd/BL/Routes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MBBackEnd.BL
+{
+    public partial class Route
+    {
+        public static List<Route> GetRoutes()
+        {
+            var context = new MajicBusEntities();
+            return (from r in context.Routes
+                    select r).ToList();
+        }
+    }
+}
