74c4fe866d40459c70e9bdc5d3e86234ab463995
Bronson <bouchardbronson@gmail.com>
2016-03-03 14:57:24 -0800

    Added internet permission and changed map marker to Kelowna

--- BusGPSTracker/GPSAPP/app/src/main/java/majicbus/gpsapp/MapsActivity.java
+++ BusGPSTracker/GPSAPP/app/src/main/java/majicbus/gpsapp/MapsActivity.java
@@ -5,8 +5,11 @@ import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -38,9 +41,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Add a marker in Kelowna and move the camera
+        LatLng Kelowna = new LatLng(49.887952, -119.496011);
+        mMap.addMarker(new MarkerOptions().position(Kelowna).title("Marker in Kelowna!"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(Kelowna));
     }
 }
