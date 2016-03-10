31fd67f78247c9cfbb1a5de615a87c1c9015f93e
Bronson <bouchardbronson@gmail.com>
2016-03-10 15:41:39 -0800

    Tried to do HTTP Get request, got some commented code in there

--- BusGPSTracker/GPSAPP/app/src/main/java/majicbus/gpsapp/MainActivity.java
+++ BusGPSTracker/GPSAPP/app/src/main/java/majicbus/gpsapp/MainActivity.java
@@ -2,32 +2,48 @@ package majicbus.gpsapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.ImageButton;
 import android.widget.TableLayout;
 import android.widget.TableRow;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 
+/* THIS DOESN'T WANT TO WORK
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+*/
+
 public class MainActivity extends AppCompatActivity {
+    private ArrayList<String> RouteData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        RouteData = new ArrayList<String>();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        //makeGetRequest(); THIS DOESN'T WANT TO WORK
+        if(RouteData.isEmpty()){
+            //Display a message cannot to connect to servers
+        }else{
+            //initTable
+        }
         initTable();
+        createMapButton();
         //createRouteLinks();
     }
 
@@ -53,8 +69,42 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void createMapButton(){
+        Button btn = (Button)findViewById(R.id.MapBtn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                intent.putStringArrayListExtra("routeData", RouteData);
+                startActivity(intent);
+            }
+        });
+    }
+
+/* THIS DOESN'T WANT TO WORK
+    private void makeGetRequest() {
+        HttpClient client = new DefaultHttpClient();
+        HttpGet request = new HttpGet("http://localhost...");
+        // replace with the url
+
+        HttpResponse response;
+        try {
+            response = client.execute(request);
+            Log.d("Response of GET request", response.toString());
+            String[] data = response.toString().split(","); //Split the JSON into individual routes
+            for(int i = 0; i < data.length; i++){
+                RouteData.add(data[i]);
+            }
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+*/
+
     public void createRouteLinks(){
-        /*
+        /* THIS IS OLD CODE THAT NEEDS TO BE ADAPTED TO BE DYNAMIC
         final ArrayList<String> routeData = new ArrayList<String>();
         routeData.add("Route 10");
         final Intent intent = new Intent(MainActivity.this, MapsActivity.class);
@@ -102,21 +152,20 @@ public class MainActivity extends AppCompatActivity {
     //Then put the data into an ArrayList or something to transfer to the map page
     public void initTable(){
         TableLayout ll = (TableLayout) findViewById(R.id.RouteTable);
-        ArrayList<String> Routes = new ArrayList<String>();
-        Routes.add("name: \"Route 97\", stops: {lat: , lng: }");
-        Routes.add("name: \"Route 10\", stops: {lat: , lng: }");
-        Routes.add("name: \"Route 8\", stops: {lat: , lng: }");
-        Routes.add("name: \"Route 1\", stops: {lat: , lng: }");
-        Routes.add("name: \"Route 23\", stops: {lat: , lng: }");;
+        RouteData.add("name: \"Route 97\", stops: {lat: , lng: }");
+        RouteData.add("name: \"Route 10\", stops: {lat: , lng: }");
+        RouteData.add("name: \"Route 8\", stops: {lat: , lng: }");
+        RouteData.add("name: \"Route 1\", stops: {lat: , lng: }");
+        RouteData.add("name: \"Route 23\", stops: {lat: , lng: }");
 
         //Still going to need to figure out how to get listeners on these then add them to an arraylist
         //when they're checked
-        for (int i = 0; i < Routes.size(); i++) {
+        for (int i = 0; i < RouteData.size(); i++) {
             TableRow row= new TableRow(this);
             TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
             row.setLayoutParams(lp);
             CheckBox routeBox = new CheckBox(this);
-            String routeName = Routes.get(i);
+            String routeName = RouteData.get(i);
             //Parse out route name
             //routeName = substring(after name: before rest)
             routeBox.setText(routeName);
