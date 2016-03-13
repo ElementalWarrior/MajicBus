925a45880c9493a255bb092cc6e6174565f43d5e
Bronson <bouchardbronson@gmail.com>
2016-03-13 16:53:50 -0700

    Preliminary HTTP request code for GPSAPP. Haven't tested. Don't know if it works.

--- BusGPSTracker/GPSAPP/app/src/main/java/majicbus/gpsapp/MainActivity.java
+++ BusGPSTracker/GPSAPP/app/src/main/java/majicbus/gpsapp/MainActivity.java
@@ -14,34 +14,23 @@ import android.widget.TableRow;
 
 import java.util.ArrayList;
 
-/* THIS DOESN'T WANT TO WORK
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-*/
-
 public class MainActivity extends AppCompatActivity {
-    private ArrayList<String> RouteData;
+    private ArrayList<String> Routes;
+    private ArrayList<String> Stops;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        RouteData = new ArrayList<String>();
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        //makeGetRequest(); THIS DOESN'T WANT TO WORK
-        if(RouteData.isEmpty()){
-            //Display a message cannot to connect to servers
-        }else{
-            //initTable
-        }
+
+        MBBackendConnection con = new MBBackendConnection(this);
+        con.makeConnection("localhost/ShowRoutesJSON");
+        Routes = con.getRouteData();
+        Stops = con.getStopData();
+
         initTable();
         createMapButton();
         //createRouteLinks();
@@ -75,33 +64,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, MapsActivity.class);
-                intent.putStringArrayListExtra("routeData", RouteData);
+                intent.putStringArrayListExtra("routeData", Routes);
+                intent.putStringArrayListExtra("stopData", Stops);
                 startActivity(intent);
             }
         });
     }
 
-/* THIS DOESN'T WANT TO WORK
-    private void makeGetRequest() {
-        HttpClient client = new DefaultHttpClient();
-        HttpGet request = new HttpGet("http://localhost...");
-        // replace with the url
-
-        HttpResponse response;
-        try {
-            response = client.execute(request);
-            Log.d("Response of GET request", response.toString());
-            String[] data = response.toString().split(","); //Split the JSON into individual routes
-            for(int i = 0; i < data.length; i++){
-                RouteData.add(data[i]);
-            }
-        } catch (ClientProtocolException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-*/
 
     public void createRouteLinks(){
         /* THIS IS OLD CODE THAT NEEDS TO BE ADAPTED TO BE DYNAMIC
@@ -152,20 +121,21 @@ public class MainActivity extends AppCompatActivity {
     //Then put the data into an ArrayList or something to transfer to the map page
     public void initTable(){
         TableLayout ll = (TableLayout) findViewById(R.id.RouteTable);
-        RouteData.add("name: \"Route 97\", stops: {lat: , lng: }");
-        RouteData.add("name: \"Route 10\", stops: {lat: , lng: }");
-        RouteData.add("name: \"Route 8\", stops: {lat: , lng: }");
-        RouteData.add("name: \"Route 1\", stops: {lat: , lng: }");
-        RouteData.add("name: \"Route 23\", stops: {lat: , lng: }");
+        //test data
+        Routes.add("name: \"Route 97\", stops: {lat: , lng: }");
+        Routes.add("name: \"Route 10\", stops: {lat: , lng: }");
+        Routes.add("name: \"Route 8\", stops: {lat: , lng: }");
+        Routes.add("name: \"Route 1\", stops: {lat: , lng: }");
+        Routes.add("name: \"Route 23\", stops: {lat: , lng: }");
 
         //Still going to need to figure out how to get listeners on these then add them to an arraylist
         //when they're checked
-        for (int i = 0; i < RouteData.size(); i++) {
+        for (int i = 0; i < Routes.size(); i++) {
             TableRow row= new TableRow(this);
             TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
             row.setLayoutParams(lp);
             CheckBox routeBox = new CheckBox(this);
-            String routeName = RouteData.get(i);
+            String routeName = Routes.get(i);
             //Parse out route name
             //routeName = substring(after name: before rest)
             routeBox.setText(routeName);
