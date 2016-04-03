54db27cb67bdcdd5fde7099b7a209a577b7c17db
James McDonnell <topgamer7@gmail.com>
2016-04-03 00:35:54 -0700

    updated scripts and added scripts to build

---
+++ DataBase/Scripts/Staging Build 2/03_ufn_dateAddOffset.sql
@@ -0,0 +1,14 @@
+if (exists select top 1 * from sys.objects where type = 'FN' and object_id = object_id('ufn_dateAddOffset'))
+begin
+	drop function ufn_dateAddOffset
+end
+
+create function ufn_dateAddOffset(@date datetime, @utcOffset varchar(50))
+returns datetime
+as 
+begin
+	declare @negative bit = case when substring(@utcOffset, 1, 1) = '-' then 1 else 0 end
+	declare @utcTime varchar(10) = case when substring(@utcOffset, 1, 1) in ('-', '+') then substring(@utcOffset, 2, len(@utcoffset)-1) else @utcOffset end
+	declare @localTime datetime = @utcTime
+	return case when @negative = 1 then @date - @localTime else @date + @localTime end
+end
